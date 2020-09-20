bf29c42d048bdeaca8dce2e4560ef225f7fcf2a9
SolarPellets <mariomario543210@gmail.com>
2020-09-20 00:02:33 -0700

    First commit

---
+++ Assets/script/banana.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class banana : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody rb;
+	public Transform target;
+	private bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    	float currentY = rb.velocity.y;
+        Vector3 forward = target.forward * -Input.GetAxis("Horizontal");
+        Vector3 right = target.right * Input.GetAxis("Vertical");
+        Vector3 v = forward + right;
+        rb.velocity = new Vector3(v.x * speed, currentY, v.z * speed);
+        
+    }
+}
---
+++ Assets/script/playButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playButton : MonoBehaviour
+{
+	public Button play;
+	public Button exit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(TaskPlay);
+        exit.onClick.AddListener(TaskExit);
+    }
+
+    // Update is called once per frame
+    void TaskPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void TaskExit(){
+    	Application.Quit();
+    }
+}
