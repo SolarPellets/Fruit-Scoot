24c072b59c368ecc4d86e3a5605285db7ff5dc79
SolarPellets <mariomario543210@gmail.com>
2020-09-29 18:32:17 -0700

    programmed jelly movement

--- Assets/script/banana.cs
+++ Assets/script/banana.cs
@@ -1,42 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class banana : MonoBehaviour
 {
-	public float speed;
-	public Rigidbody rb;
-	public Transform target;
-	private bool paused;
-	private float pauseTimer;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float speed;
+    public Rigidbody rb;
+    public Transform target;
+    private bool paused;
+    private float pauseTimer;
+    public bool jellied;
+    public bool colliding;
+
+    void Start(){
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-    	float currentY = rb.velocity.y;
+    void FixedUpdate(){
+        float currentY = rb.velocity.y;
         Vector3 forward = target.forward * -Input.GetAxis("Horizontal");
         Vector3 right = target.right * Input.GetAxis("Vertical");
         Vector3 v = forward + right;
+        if(jellied && colliding){
+            if(Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0){
+                currentY += 1;
+            }
+        }
         rb.velocity = new Vector3(v.x * speed, currentY, v.z * speed);
     }
     void Update(){
-    	if(Input.GetAxisRaw("Jump") * Time.unscaledDeltaTime > 0){
-    		if(pauseTimer < 0){
-    			paused = !paused;
-    			pauseTimer = 3;
-    		}
-    	}
-    	if(paused == true){
-    		Time.timeScale = 0;
-    	}
-    	else{
-    		Time.timeScale = 1;
-    	}
-    	pauseTimer -= Time.unscaledDeltaTime;
+        if(Input.GetAxisRaw("Jump") * Time.unscaledDeltaTime > 0){
+            if(pauseTimer < 0){
+                paused = !paused;
+                pauseTimer = 0.5f;
+            }
+        }
+        if(paused == true){
+            Time.timeScale = 0;
+        }
+        else{
+            Time.timeScale = 1;
+        }
+        pauseTimer -= Time.unscaledDeltaTime;
+    }
+    void OnCollisionEnter(Collision collision){
+        colliding = true;
+    }
+    void OnCollisionExit(Collision collision){
+        colliding = false;
     }
 }
